0f088e65cc30e7ab16f9825e1669ccf88b12a661
王兴 <2950742557@qq.com>
2023-07-13 11:58:00 +0800

    删除权限缓存

--- dy-apps/dy-service-api/dy-service-base/src/main/java/com/neu/dy/api/TransportLineFeign.java
+++ dy-apps/dy-service-api/dy-service-base/src/main/java/com/neu/dy/api/TransportLineFeign.java
@@ -1,11 +1,9 @@
 package com.neu.dy.api;
 
 import com.neu.dy.base.dto.transportline.TransportLineDto;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,10 @@ public interface TransportLineFeign {
 
     @GetMapping("/{id}")
     public TransportLineDto fineById(@PathVariable(name = "id") String id);
+
+    @GetMapping("")
+    @ApiOperation(value = "根据id、机构id、机构ids获取线路列表")
+    public List<TransportLineDto> findAll(@RequestParam(name = "ids", required = false) List<String> ids,
+                                          @RequestParam(name = "agencyId", required = false) String agencyId,
+                                          @RequestParam(name = "agencyIds", required = false) List<String> agencyIds);
 }
