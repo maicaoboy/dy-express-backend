15b0be2b2ef227cc89c8adec5e30cee2e97a755d
宏彬 李 <L-hongbin@outlook.com>
2023-07-18 11:29:58 +0800

    添加测试

---
+++ dy-apps/dy-auth/dy-auth-server/src/main/test/java/com/neu/dy/authority/biz/service/auth/ValidateCodeServiceTest.java
@@ -0,0 +1,23 @@
+package com.neu.dy.authority.biz.service.auth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Classname ValidateCodeServiceTest
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2023/7/18 10:39
+ * @Created by maicaoboy
+ */
+class ValidateCodeServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void check() {
+    }
+}
